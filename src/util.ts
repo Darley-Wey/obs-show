296e9b5580b8b1441a5168b7be2a4c5ee5f03648
Darley <darley.wey@gmail.com>
2024-11-25 18:14:23 +0800

    feat(util): 添加米转像素的函数

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import {store} from "./config";
+
 export function formatLength(length: number, decimalPlaces: number,): string {
     const decimalHelper = Math.pow(10, decimalPlaces);
     let output;
@@ -9,4 +11,16 @@ export function formatLength(length: number, decimalPlaces: number,): string {
         output = (Math.round(length * 100 * decimalHelper) / decimalHelper) + ' cm';
     }
     return output;
+}
+
+export function metersToPixels(meters: number): number {
+    const map = store.map;
+    if (!map) {
+        return 0;
+    }
+    const resolution = map.getView().getResolution();
+    if (resolution !== undefined) {
+        return meters / resolution;
+    }
+    return 0;
 }
