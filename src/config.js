50a230b4b315ce04f591039eacea3702dce988c1
Darley <darley.wey@gmail.com>
2024-11-25 18:14:07 +0800

    feat(config): 添加颜色常量和地图状态管理对象

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const red = '#ff0000';
+export const transparentRed = 'rgba(255, 0, 0, 0.05)';
+export const blue = '#00c0ff';
+export const transparentBlue = 'rgba(0, 192, 255, 0.05)';
+export const store= {
+    map: null,
+    showTrails: true, // F1切换
+    useDMS: false, // F2切换
+    showRedCircleAndSector: false, // F3切换
+    showBlueCircleAndSector: false, // F4切换
+}
