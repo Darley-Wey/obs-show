4f1c06772c0b5fef538e74db25ac79f342bff585
Darley <darley.wey@gmail.com>
2024-12-03 10:06:39 +0800

    feat(util): 修正米到像素的转换计算方法

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,6 @@
 import {store} from "./config";
+import olMap from "ol/Map";
+import {getPointResolution} from "ol/proj";
 
 export function formatLength(length: number, decimalPlaces: number,): string {
     const decimalHelper = Math.pow(10, decimalPlaces);
@@ -14,13 +16,16 @@ export function formatLength(length: number, decimalPlaces: number,): string {
 }
 
 export function metersToPixels(meters: number): number {
-    const map = store.map;
+    const map: olMap = store.map;
     if (!map) {
         return 0;
     }
+    const center = map.getView().getCenter();
     const resolution = map.getView().getResolution();
+    const pointResolution = getPointResolution('EPSG:3857', resolution, center);
+    const dpr = window.devicePixelRatio;
     if (resolution !== undefined) {
-        return meters / resolution;
+        return meters / pointResolution * dpr;
     }
     return 0;
 }
